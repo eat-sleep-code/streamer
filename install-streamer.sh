76941342a5561653debb720c81765c739a2421ef
eat-sleep-code <info@eat-sleep-code.com>
2021-05-27 08:30:35 -0700

    Force update of PIP libraries

--- install-streamer.sh
+++ install-streamer.sh
@@ -9,7 +9,7 @@ sudo apt update
 
 echo ''
 echo -e '\033[93mInstalling prerequisites... \033[0m'
-sudo apt install -y git python3 python3-pip screen ffmpeg
+sudo apt install -y git python3 python3-pip screen ffmpeg --force
 sudo pip3 install keyboard 
 
 echo '\033[93mProvisioning logs... \033[0m'
